42f88833dca0c0f85d075e72ce90d5862cfeb81f
Rathd Pruthviraj <145155770+Rathodpruthvirajsing@users.noreply.github.com>
2025-06-17 13:45:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-//your JS code here. If required.
+const progress = document.getElementById('progress');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const circles = document.querySelectorAll('.circle');
+
+let currentStep = 1;
+
+next.addEventListener('click', () => {
+  currentStep++;
+
+  if (currentStep > circles.length) {
+    currentStep = circles.length;
+  }
+
+  update();
+});
+
+prev.addEventListener('click', () => {
+  currentStep--;
+
+  if (currentStep < 1) {
+    currentStep = 1;
+  }
+
+  update();
+});
+
+function update() {
+  circles.forEach((circle, index) => {
+    if (index < currentStep) {
+      circle.classList.add('active');
+    } else {
+      circle.classList.remove('active');
+    }
+  });
+
+  const activeCircles = document.querySelectorAll('.circle.active');
+  const progressWidth = ((activeCircles.length - 1) / (circles.length - 1)) * 100;
+  progress.style.width = progressWidth + '%';
+
+  prev.disabled = currentStep === 1;
+  next.disabled = currentStep === circles.length;
+}
